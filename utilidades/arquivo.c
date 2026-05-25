0d50c0fa61d50cdca67559d9f3c710c4e0b7f3d8
Gabriel Perin <gvp24@inf.ufpr.br>
2026-05-25 13:06:45 -0300

    adicionado envio e recebimento de arquivos

--- utilidades/arquivo.c
+++ utilidades/arquivo.c
@@ -4,41 +4,72 @@
 #include <protocolo.h>
 #include <rede.h>
 
-static void arquivo_envia(FILE *arquivo, int soquete)
+static void envia_dados(FILE *arquivo, int soquete)
 {
     struct pacote dados;
     uint8_t buffer[31];
-    uint8_t tamBuffer;
+    uint8_t tamBuffer = fread(buffer, sizeof(uint8_t), 31, arquivo);
 
-    do {
-        tamBuffer = fread(buffer, sizeof(uint8_t), 31, arquivo);
+    while(tamBuffer > 0) {
         constroi_pacote(&dados, tamBuffer, TIPO_DADOS, buffer);
         rede_envia(&dados, soquete);
-    } while(tamBuffer != 0);
+        tamBuffer = fread(buffer, sizeof(uint8_t), 31, arquivo);
+    }
 
     constroi_pacote(&dados, 0, TIPO_FIM, NULL);
     rede_envia(&dados, soquete);
 }
 
-void arquivo_recebe(FILE *arquivo, int soquete)
+static int arquivo_envia(char *nomeArquivo, int tipoPacote, int soquete)
+{
+    if(strlen(nomeArquivo) + 1 > MAX_6BIT)
+        return 2;
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if(arquivo == NULL)
+        return 1;
+
+    struct pacote tipoArquivo;
+    constroi_pacote(&tipoArquivo, strlen(nomeArquivo) + 1, tipoPacote, (uint8_t *) nomeArquivo);
+    rede_envia(&tipoArquivo, soquete);
+    envia_dados(arquivo, soquete);
+
+    return 0;
+}
+
+void arquivo_recebe(int soquete)
 {
     struct pacote dados;
     rede_escuta(&dados, soquete);
-    while(dados.tipo != TIPO_FIM) {
-        fwrite(dados.dados, sizeof(uint8_t), dados.tamanho, arquivo);
+    char nomeArquivo[dados.tamanho];
+    strcpy(nomeArquivo, (char *) dados.dados);
+    FILE *arquivo = fopen(nomeArquivo, "w");
+
+    do {
         rede_escuta(&dados, soquete);
-    }
+        fwrite(dados.dados, sizeof(uint8_t), dados.tamanho, arquivo);
+    } while(dados.tipo != TIPO_FIM);
 }
 
+// Envia arquivo MP4
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
 int envia_mp4(char *nomeArquivo, int soquete)
 {
-    FILE *arquivo = fopen(nomeArquivo, "r");
-    struct pacote tipoArquivo;
-    if(strlen(nomeArquivo) + 1 > MAX_6BIT)
-        return -1;
-    constroi_pacote(&tipoArquivo, strlen(nomeArquivo) + 1, TIPO_MP4, (uint8_t *) nomeArquivo);
-    rede_envia(&tipoArquivo, soquete);
-    arquivo_envia(arquivo, soquete);
+    return arquivo_envia(nomeArquivo, TIPO_MP4, soquete);
+}
 
-    return 0;
+// Envia arquivo TXT
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
+int envia_txt(char *nomeArquivo, int soquete)
+{
+    return arquivo_envia(nomeArquivo, TIPO_TXT, soquete);
+}
+
+// Envia arquivo JPG
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
+int envia_jpg(char *nomeArquivo, int soquete)
+{
+    return arquivo_envia(nomeArquivo, TIPO_JPG, soquete);
 }
---
+++ utilidades/arquivo.h
@@ -0,0 +1,22 @@
+#ifndef ARQUIVO_H
+#define ARQUIVO_H
+
+// Envia arquivo MP4
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
+int envia_mp4(char *nomeArquivo, int soquete);
+
+// Envia arquivo TXT
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
+int envia_txt(char *nomeArquivo, int soquete);
+
+// Envia arquivo JPG
+// Retorna 1 caso o arquivo não exista
+// Retorna 2 caso o nome do arquivo tenha mais que 63 caracteres
+int envia_jpg(char *nomeArquivo, int soquete);
+
+// Cria um arquivo com o nome enviado e escreve o conteúdo
+void arquivo_recebe(int soquete);
+
+#endif
